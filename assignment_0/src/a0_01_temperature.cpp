f3a3b1244915e3d480cee2b24f1c48a71196c59e
Rui <1622146395@qq.com>
2026-02-20 23:00:14 +0800

    0.1

--- assignment_0/src/a0_01_temperature.cpp
+++ assignment_0/src/a0_01_temperature.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
-#include "rm_a0/a0_01_temperature.hpp"
+#include <cstdio>
+#include "/home/wr1/文档/GitHub/assessment_winter_2026/assignment_0/include/rm_a0/a0_01_temperature.hpp"
 
 int main() {
   double celsius = 0.0;
@@ -9,4 +9,14 @@ int main() {
   }
   std::cout << rm_a0::FormatFahrenheit(rm_a0::CelsiusToFahrenheit(celsius));
   return 0;
+}
+
+double CelsiusToFahrenheit(double celsius) {
+    return celsius * 9.0 / 5.0 + 32.0;
+}
+
+std::string FormatFahrenheit(double fahrenheit) {
+    char buffer[100];
+    snprintf(buffer, sizeof(buffer), "%.2f\n", fahrenheit);
+    return std::string(buffer);
 }
